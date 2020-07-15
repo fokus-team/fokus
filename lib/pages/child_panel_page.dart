3d90a1dacc08d668493cb2727c875e547a7847ea
Mateusz <37753289+Evil00787@users.noreply.github.com>
2020-07-15 17:17:59 +0200

    Added pl and en

--- lib/pages/child_panel_page.dart
+++ lib/pages/child_panel_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fokus/data/model/user/caregiver.dart';
 import 'package:fokus/utils/app_locales.dart';
 import 'package:fokus/utils/theme_config.dart';
 import 'package:fokus/wigets/plan_widget.dart';
@@ -16,8 +15,6 @@ class _ChildPanelPageState extends State<ChildPanelPage> {
 
   @override
   Widget build(BuildContext context) {
-    var user = ModalRoute.of(context).settings.arguments as Caregiver;
-
     return Scaffold(
         bottomNavigationBar: BottomNavigationBar(
           selectedItemColor: AppColors.childBackgroundColor,
@@ -26,33 +23,85 @@ class _ChildPanelPageState extends State<ChildPanelPage> {
             BottomNavigationBarItem(
                 icon: Icon(Icons.format_align_justify),
                 activeIcon: Icon(Icons.format_align_justify),
-                title: Text('${AppLocales.of(context).translate("page.child.bottomNavBar.plans")} ')),
+                title: Text(
+                    '${AppLocales.of(context).translate("page.child.bottomNavBar.plans")} ')),
             BottomNavigationBarItem(
                 icon: Icon(Icons.star),
                 activeIcon: Icon(Icons.star),
-                title: Text('${AppLocales.of(context).translate("page.child.bottomNavBar.rewards")} ')),
+                title: Text(
+                    '${AppLocales.of(context).translate("page.child.bottomNavBar.rewards")} ')),
             BottomNavigationBarItem(
                 icon: Icon(Icons.flag),
                 activeIcon: Icon(Icons.flag),
-                title: Text('${AppLocales.of(context).translate("page.child.bottomNavBar.achievements")} '))
+                title: Text(
+                    '${AppLocales.of(context).translate("page.child.bottomNavBar.achievements")} '))
           ],
         ),
         body: Container(
             child: SafeArea(
-                child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+                child: ListView(
+									physics: BouncingScrollPhysics(),
           children: <Widget>[
             Padding(
                 padding: EdgeInsets.symmetric(
                     vertical: AppBoxProperties.sectionPadding,
                     horizontal: AppBoxProperties.screenEdgePadding),
-                child: Text('${AppLocales.of(context).translate("page.child.childPanel.todaysPlans")} ',
+                child: Text(
+                    '${AppLocales.of(context).translate("page.child.childPanel.inProgress")} ',
+                    style: Theme.of(context).textTheme.headline1)),
+            PlanWidget("Nauka mnożenia", true, 6, true, 0.2,
+							"Pozostało: 20 minut"),
+            Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: AppBoxProperties.sectionPadding,
+                    horizontal: AppBoxProperties.screenEdgePadding),
+                child: Text(
+                    '${AppLocales.of(context).translate("page.child.childPanel.todaysPlans")} ',
                     style: Theme.of(context).textTheme.headline1)),
             PlanWidget(
                 "Gra w piłkę nożną pośrodku ulicy skąpanej w promieniach błogiego słońca wyróżniającego się słodkim zapachem nieba",
                 false,
-                2),
-            PlanWidget("Gra w piłkę nożną", true, 4),
+                2,
+                false,
+                0,
+							"Codziennie"),
+            PlanWidget("Gra w piłkę nożną", true, 4, false, 0,
+							"Co każdy wtorek, piątek"),
+            PlanWidget(
+                "Gra w piłkę nożną pośrodku ulicy skąpanej w promieniach błogiego słońca wyróżniającego się słodkim zapachem nieba",
+                false,
+                2,
+                false,
+                0,
+							"Codziennie"),
+            PlanWidget(
+                "Gra w piłkę nożną pośrodku ulicy skąpanej w promieniach błogiego słońca wyróżniającego się słodkim zapachem nieba",
+                false,
+                2,
+                false,
+                0,
+							"Codziennie"),
+            PlanWidget(
+                "Gra w piłkę nożną pośrodku ulicy skąpanej w promieniach błogiego słońca wyróżniającego się słodkim zapachem nieba",
+                false,
+                2,
+                false,
+                0,
+							"Codziennie"),
+            PlanWidget(
+                "Gra w piłkę nożną pośrodku ulicy skąpanej w promieniach błogiego słońca wyróżniającego się słodkim zapachem nieba",
+                false,
+                2,
+                false,
+                0,
+							"Codziennie"),
+            PlanWidget(
+                "Gra w piłkę nożną pośrodku ulicy skąpanej w promieniach błogiego słońca wyróżniającego się słodkim zapachem nieba",
+                false,
+                2,
+                false,
+                0,
+						"Codziennie"),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
@@ -75,7 +124,7 @@ class _ChildPanelPageState extends State<ChildPanelPage> {
                             color: Colors.white,
                           ),
                           Text(
-														'${AppLocales.of(context).translate("page.child.childPanel.futurePlans")} ',
+                            '${AppLocales.of(context).translate("page.child.childPanel.futurePlans")} ',
                             style: Theme.of(context).textTheme.button,
                           )
                         ])))
--- lib/wigets/plan_widget.dart
+++ lib/wigets/plan_widget.dart
@@ -5,9 +5,10 @@ import 'package:fokus/utils/app_locales.dart';
 import 'package:fokus/utils/theme_config.dart';
 
 class PlanWidget extends StatelessWidget {
-  final String planName;
-  final bool isActive;
+  final String planName, details;
+  final bool isActive, inProgress;
   final int tasksCount;
+  final double progressPercentage;
 
   // styling vars
   final double playButtonSize = 40.0;
@@ -18,8 +19,12 @@ class PlanWidget extends StatelessWidget {
   final int titleMaxLines = 3;
   final Color activeButtonColor = Colors.teal;
   final Color disabledButtonColor = Colors.grey;
+  final Color inProgressButtonColor = Colors.amber;
+  final Icon notInProgressIcon = new Icon(Icons.play_arrow);
+  final Icon inProgressIcon = new Icon(Icons.launch);
 
-  PlanWidget(this.planName, this.isActive, this.tasksCount);
+  PlanWidget(this.planName, this.isActive, this.tasksCount, this.inProgress,
+      this.progressPercentage, this.details);
 
   @override
   Widget build(BuildContext context) {
@@ -35,44 +40,54 @@ class PlanWidget extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Flexible(
-                    child: Padding(
-                  padding: EdgeInsets.all(AppBoxProperties.containerPadding),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(
-                        this.planName,
-                        style: Theme.of(context).textTheme.headline2,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: titleMaxLines,
-                      ),
                       Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: AppBoxProperties.columnChildrenPadding),
-                          child: Text(
-                            "Ok",
-                            style: Theme.of(context).textTheme.subtitle1,
-                          )),
+                          padding: EdgeInsets.all(AppBoxProperties.containerPadding),
+
+                          child: Column(crossAxisAlignment: CrossAxisAlignment.start,children: <Widget>[
+                            Text(
+                              this.planName,
+                              style: Theme.of(context).textTheme.headline2,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: titleMaxLines,
+                            ),
+                            Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical:
+                                        AppBoxProperties.columnChildrenPadding),
+                                child: Text(
+                                  this.details,
+                                  style: Theme.of(context).textTheme.subtitle1,
+                                )),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Padding(
+                                    padding: EdgeInsets.only(
+                                        right:
+                                            AppBoxProperties.buttonIconPadding),
+                                    child: Icon(
+                                      Icons.speaker_notes,
+                                      color: Colors.blue,
+                                      size: tasksIconSize,
+                                    )),
+                                Text(
+                                    this.tasksCount.toString() +
+                                        ' ${AppLocales.of(context).translate("page.child.childPanel.tasks")} ',
+                                    style: blueTextStyle)
+                              ],
+                            )
+                          ])),
                       Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Padding(
-                              padding: EdgeInsets.only(
-                                  right: AppBoxProperties.buttonIconPadding),
-                              child: Icon(
-                                Icons.speaker_notes,
-                                color: Colors.blue,
-                                size: tasksIconSize,
-                              )),
-                          Text(
-                              this.tasksCount.toString() +
-                                  ' ${AppLocales.of(context).translate("page.child.childPanel.tasks")} ',
-                              style: blueTextStyle)
+                          Expanded(child: addProgressIndicatorIfAvailable())
                         ],
                       )
                     ],
                   ),
-                )),
+                ),
                 Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.horizontal(
@@ -85,7 +100,7 @@ class PlanWidget extends StatelessWidget {
                         child: IconButton(
                       color: Colors.white,
                       disabledColor: Colors.white,
-                      icon: Icon(Icons.play_arrow),
+                      icon: chooseIcon(),
                       iconSize: playButtonSize,
                       onPressed: () {},
                     )))
@@ -94,6 +109,27 @@ class PlanWidget extends StatelessWidget {
   }
 
   Color chooseColor() {
+    if (this.inProgress) return inProgressButtonColor;
     return this.isActive ? activeButtonColor : disabledButtonColor;
+  }
+
+  Icon chooseIcon() {
+    return this.inProgress ? inProgressIcon : notInProgressIcon;
+  }
+
+  Widget addProgressIndicatorIfAvailable() {
+    if (!inProgress)
+      return Container();
+    else
+      return ClipRRect(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(5)),
+              child: Container(
+                height: 15,
+                child: LinearProgressIndicator(
+                    value: this.progressPercentage,
+                    backgroundColor: Color.fromARGB(255, 224, 224, 224),
+                    valueColor:
+                        new AlwaysStoppedAnimation<Color>(Colors.lightGreen)),
+              ));
   }
 }

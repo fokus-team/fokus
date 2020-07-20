7286b897d4a97c9e2f770496ead1a702745224f9
Mateusz <37753289+Evil00787@users.noreply.github.com>
2020-07-20 20:10:12 +0200

    Small adjustments

--- lib/pages/child/panel_page.dart
+++ lib/pages/child/panel_page.dart
@@ -1,7 +1,5 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:fokus/logic/active_user/active_user_cubit.dart';
-import 'package:fokus/model/ui/ui_child.dart';
 import 'package:fokus/utils/app_locales.dart';
 import 'package:fokus/utils/theme_config.dart';
 
@@ -57,35 +55,35 @@ class _ChildPanelPageState extends State<ChildPanelPage> {
   }
 
   List<Segment> _buildPanelSegments(ChildPlansLoadSuccess state) {
-    var user =
-        ((CubitProvider.of<ActiveUserCubit>(context).state as ActiveUserPresent)
-            .user) as UIChild;
     return [
-      if (user.hasActivePlan)
+			if(state.activePlan != null)
         Segment(title: '$_pageKey.content.inProgress', elements: <Widget>[
-          for (var plan in state.plans)
             ItemCard(
                 actionButton: ItemCardActionButton(
                     icon: Icons.launch,
                     color: AppColors.childOpenActivePlanColor,
                     onTapped: () => {log("startPlan")}),
-                title: plan.name,
-                subtitle: plan.description(context),
+                title: state.activePlan.name,
+                subtitle: state.activePlan.description(context),
+                isActive: true,
+                //TODO: add progress from DB
+                progressPercentage: 0.4,
                 chips: <Widget>[
                   AttributeChip.withIcon(
                       icon: Icons.description,
                       color: AppColors.childPlanTaskCountChipColor,
                       content:
                           ' ${AppLocales.of(context).translate("page.childSection.panel.content.tasks", {
-                        'NUM_TASKS': plan.taskCount
+                        'NUM_TASKS': state.activePlan.taskCount
                       })}')
                 ])
         ]),
       Segment(
-          title: '$_pageKey.content.todaysPlans',
+          title: state.activePlan == null ? '$_pageKey.content.todaysPlans' : '$_pageKey.content.remainingTodaysPlans',
           noElementsMessage: '$_pageKey.content.noPlans',
           elements: <Widget>[
             for (var plan in state.plans)
+            	if(plan.id != state.activePlan.id)
               ItemCard(
                   actionButton: ItemCardActionButton(
                       icon: Icons.play_arrow,

0125c3510d9a11793449b8d83400659ede1ebad0
Mateusz <37753289+Evil00787@users.noreply.github.com>
2020-08-01 16:09:52 +0200

    First changes

--- lib/logic/child_tasks/child_tasks_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'child_tasks_state.dart';
-
-class ChildTasksCubit extends Cubit<ChildTasksState> {
-  ChildTasksCubit() : super(ChildTasksInitial());
-}

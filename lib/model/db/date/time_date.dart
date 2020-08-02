ec53106ca162d75fb37bcf493cc1ca2622563f2c
Stanisław Góra <stasgora@gmail.com>
2020-08-02 20:11:50 +0200

    Date constructor typo

--- lib/model/db/date/time_date.dart
+++ lib/model/db/date/time_date.dart
@@ -3,7 +3,7 @@ import 'date_base.dart';
 class TimeDate extends DateBase {
   TimeDate(int year, int month, int day, [int hour = 0, int minute = 0, int second = 0]) : super(year, month, day, hour, minute, second);
 
-  TimeDate.fromDate(DateTime date) : super(date.year, date.month, date.day, date.hour, date.day, date.second);
+  TimeDate.fromDate(DateTime date) : super(date.year, date.month, date.day, date.hour, date.minute, date.second);
 
   TimeDate.now() : this.fromDate(DateTime.now());
 

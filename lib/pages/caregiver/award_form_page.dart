1a1a9797571922bbde91dee40ef91e9fc8feaada
Mikolaj Mirko <mikolajmirko@gmail.com>
2020-08-22 14:05:25 +0200

    icon picker widget, point picker used

--- lib/pages/caregiver/award_form_page.dart
+++ lib/pages/caregiver/award_form_page.dart
@@ -7,6 +7,8 @@ import 'package:fokus/model/ui/award/ui_award.dart';
 import 'package:fokus/model/ui/ui_currency.dart';
 import 'package:fokus/utils/icon_sets.dart';
 import 'package:fokus/widgets/buttons/help_icon_button.dart';
+import 'package:fokus/widgets/forms/iconpicker_field.dart';
+import 'package:fokus/widgets/forms/pointpicker_field.dart';
 import 'package:smart_select/smart_select.dart';
 
 import 'package:fokus/services/app_locales.dart';
@@ -219,155 +221,40 @@ class _CaregiverAwardFormPageState extends State<CaregiverAwardFormPage> {
 	}
 	
 	Widget buildPointsFields(BuildContext context) {
-		return SmartSelect<UICurrency>.single(
-			builder: (context, state, function) {
-				return Padding(
-					padding: EdgeInsets.only(top: 0.0, bottom: 0.0, left: 20.0, right: 16.0),
-					child: Row(
-						crossAxisAlignment: CrossAxisAlignment.start,
-						children: [
-							Expanded(
-								child: TextFormField(
-									controller: _pointsController,
-									decoration: InputDecoration(
-										icon: Padding(padding: EdgeInsets.all(5.0), child: Icon(Icons.star)),
-										contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
-										border: OutlineInputBorder(),
-										hintText: "0",
-										helperMaxLines: 3,
-										errorMaxLines: 3,
-										helperText: AppLocales.of(context).translate('$_pageKey.fields.awardPoints.hint'),
-										labelText: AppLocales.of(context).translate('$_pageKey.fields.awardPoints.valueLabel'),
-										suffixIcon: IconButton(
-											onPressed: () {
-												FocusScope.of(context).requestFocus(FocusNode());
-												_pointsController.clear();
-											},
-											icon: Icon(Icons.clear)
-										)
-									),
-									validator: (value) {
-										final range = [1, 1000000];
-										final int numValue = int.tryParse(value);
-										return (numValue != null && (numValue < range[0] || numValue > range[1])) ? 
-											AppLocales.of(context).translate('alert.genericRangeOverflowValue', {'A': range[0].toString(), 'B': range[1].toString()})
-											: null;
-									},
-									keyboardType: TextInputType.numberWithOptions(signed: true, decimal: false),
-									inputFormatters: <TextInputFormatter>[
-											WhitelistingTextInputFormatter.digitsOnly
-									],
-									onChanged: (val) => setState(() {
-										award.pointValue = int.tryParse(val);
-										isDataChanged = true;
-									})
-								)
-							),
-							GestureDetector(
-								onTap: () {
-									if(currencies.length > 1) {
-										FocusManager.instance.primaryFocus.unfocus();
-										function(context);
-									}
-								},
-								child: Tooltip(
-									message: AppLocales.of(context).translate('$_pageKey.fields.awardPoints.currencyLabel'),
-									child: Row(
-										children: <Widget>[
-											Padding(
-												padding: EdgeInsets.only(left: 10.0, top: 4.0),
-												child: CircleAvatar(
-													child: SvgPicture.asset(currencySvgPath(state.value.type), width: 28, fit: BoxFit.cover),
-													backgroundColor: AppColors.currencyColor[state.value.type].withAlpha(50)
-												)
-											),
-											if(currencies.length > 1) 
-												Padding(
-													padding: EdgeInsets.only(left: 4.0, top: 2.0),
-													child: Icon(Icons.keyboard_arrow_right, color: Colors.grey)
-												)
-										]
-									)
-								)
-							)
-						]
-					)
-				);
+		return PointPickerField(
+			controller: _pointsController,
+			pickedCurrency: award.pointCurrency,
+			currencies: currencies,
+			loading: false,
+			minPoints: 1,
+			canBeEmpty: false,
+			labelValueText: AppLocales.of(context).translate('$_pageKey.fields.awardPoints.valueLabel'),
+			helperValueText: AppLocales.of(context).translate('$_pageKey.fields.awardPoints.hint'),
+			labelCurrencyText: AppLocales.of(context).translate('$_pageKey.fields.awardPoints.currencyLabel'),
+			pointValueSetter: (val) {
+				setState(() {
+					isDataChanged = award.pointValue != ((val != null) ? int.tryParse(val) : null);
+					award.pointValue = (val != null) ? int.tryParse(val) : null;
+				});
 			},
-			title: AppLocales.of(context).translate('$_pageKey.fields.awardPoints.currencyLabel'),
-			value: award.pointCurrency,
-			options: [
-				for(UICurrency element in currencies)
-					SmartSelectOption(
-						title: element.title,
-						value: element
-					)
-			],
-			choiceConfig: SmartSelectChoiceConfig(
-				builder: (item, checked, onChange) {
-					return RadioListTile<UICurrency>(
-						value: item.value,
-						groupValue: award.pointCurrency,
-						onChanged: (val) => {onChange(item.value, !checked)},
-						title: Row(
-							children: [
-								Padding(
-									padding: EdgeInsets.only(right: 6.0),
-									child: SvgPicture.asset(currencySvgPath(item.value.type), width: 30, fit: BoxFit.cover)
-								),
-								Text(item.value.title, style: TextStyle(fontWeight: FontWeight.bold, color: AppColors.currencyColor[item.value.type]))
-							]
-						)
-					);
-				}
-			),
-			modalType: SmartSelectModalType.bottomSheet,
-			onChange: (val) => setState(() => award.pointCurrency = val)
+			pointCurrencySetter: (val) {
+				setState(() {
+					isDataChanged = award.pointCurrency != val;
+					award.pointCurrency = val;
+				});
+			},
 		);
 	}
 
 	Widget buildIconField(BuildContext context) {
-		return Padding(
-			padding: EdgeInsets.symmetric(vertical: 10.0),
-			child: SmartSelect<int>.single(
-				leading: SvgPicture.asset(awardIconSvgPath(award.icon), height: 74.0),
-				title: AppLocales.of(context).translate('$_pageKey.fields.awardIcon.label'),
-				value: award.icon,
-				options: List.generate(awardIcons.length, (index) {
-						final String name = AppLocales.of(context).translate('$_pageKey.fields.awardIcon.groups.${awardIcons[index].label.toString().split('.').last}');
-						return SmartSelectOption(
-							title: name,
-							group: name,
-							value: index
-						);
-					}
-				),
-				isTwoLine: true,
-				choiceConfig: SmartSelectChoiceConfig(
-					glowingOverscrollIndicatorColor: Colors.teal,
-					runSpacing: 10.0,
-					spacing: 10.0,
-					useWrap: true,
-					isGrouped: true,
-					builder: (item, checked, onChange) {
-						return Badge(
-							badgeContent: Icon(Icons.check, color: Colors.white, size: 16.0),
-							badgeColor: Colors.green,
-							animationType: BadgeAnimationType.scale,
-							showBadge: checked != null ? checked : false,
-							child: GestureDetector(
-								onTap: () => { onChange(item.value, !checked) },
-								child: SvgPicture.asset(awardIconSvgPath(item.value), height: 64.0)
-							)
-						);
-					}
-				),
-				modalType: SmartSelectModalType.bottomSheet,
-				onChange: (val) => setState(() {
-					FocusManager.instance.primaryFocus.unfocus();
-					award.icon = val;
-				})
-			)
+		return IconPickerField.award(
+			title: AppLocales.of(context).translate('$_pageKey.fields.awardIcon.label'),
+			groupTextKey: '$_pageKey.fields.awardIcon.groups',
+			value: award.icon,
+			callback: (val) => setState(() {
+				FocusManager.instance.primaryFocus.unfocus();
+				award.icon = val;
+			})
 		);
 	}
 

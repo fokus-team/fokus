ddf3a646e8374c01828ba22fb4848b47e824519d
Stanisław Góra <stasgora@gmail.com>
2020-08-23 21:39:50 +0200

    UI child filtering support

--- lib/logic/calendar_cubit.dart
+++ lib/logic/calendar_cubit.dart
@@ -20,63 +20,88 @@ import 'package:fokus/utils/string_utils.dart';
 class CalendarCubit extends Cubit<CalendarState> {
 	final ActiveUserFunction _activeUser;
 	Map<ObjectId, Plan> _plans;
-	Map<ObjectId, String> _children;
+	Map<ObjectId, String> _childNames;
+	Map<Date, Map<Date, List<UIPlan>>> _allEvents;
 
 	final DataRepository _dataRepository = GetIt.I<DataRepository>();
 	final PlanRepeatabilityService _repeatabilityService = GetIt.I<PlanRepeatabilityService>();
 
   CalendarCubit(this._activeUser) : super(CalendarState());
+  
+  void childFilterChanged(Map<UIUser, bool> filter) async => emit(state.copyWith(children: filter, events: await _filterData(filter, state.month)));
 
-  void loadDataForMonth(Date month) async {
+  void monthChanged(Date month) async => emit(state.copyWith(month: month, events: await _filterData(state.children, month)));
+
+	Future<Map<Date, List<UIPlan>>> _filterData(Map<UIUser, bool> filter, Date month) async {
+		var ids = filter.keys.map((child) => child.id).toSet();
+		Map<Date, List<UIPlan>> events = {};
+		if (ids.length > 0) {
+			var monthEvents = _allEvents[state.month] ?? await _loadDataForMonth(state.month);
+			for (var day in monthEvents.entries) {
+				List<UIPlan> plans = [];
+				for (var plan in day.value)
+					if (ids.any(plan.assignedTo.contains))
+						plans.add(plan.copyWith(description: _getDescription(plan.assignedTo.where(ids.contains))));
+				events[day.key] = plans;
+			}
+		}
+		return events;
+	}
+
+	Future<Map<Date, List<UIPlan>>> _loadDataForMonth(Date month) async {
 	  var activeUser = _activeUser() as UICaregiver;
-	  if (state.events.containsKey(month))
-	  	return;
+	  if (_allEvents.containsKey(month))
+	  	return _allEvents[month];
 	  if (_plans == null)
 	    _plans = Map.fromEntries((await _dataRepository.getPlans(caregiverId: activeUser.id, activeOnly: false)).map((plan) => MapEntry(plan.id, plan)));
-	  if (_children == null)
-	  	_children = await _dataRepository.getUserNames(activeUser.connections);
+	  var state = this.state;
+	  if (_childNames == null) {
+	  	var children = await _dataRepository.getUsers(ids: activeUser.connections);
+	  	state = state.copyWith(children: Map.fromEntries(children.map((child) => MapEntry(UIUser.fromDBModel(child), true))));
+		  _childNames = Map.fromEntries(children.map((child) => MapEntry(child.id, child.name)));
+	  }
 	  var currentMonth = Date.fromDate(Utils.firstDayOfMonth(Date.now()));
 
 	  Map<Date, List<UIPlan>> events = {};
 	  if (month < currentMonth)
-		  events.addAll(loadFutureData(getMonthSpan(month)));
+		  events.addAll(_loadFutureData(_getMonthSpan(month)));
 	  else if (month > currentMonth)
-		  events.addAll(loadFutureData(getMonthSpan(month)));
+		  events.addAll(_loadFutureData(_getMonthSpan(month)));
 	  else {
-		  events.addAll(await loadPastData(DateSpan(from: month, to: Date.now()))); // TODO handle today better
-		  events.addAll(loadFutureData(DateSpan(from: Date.now(), to: Utils.nextMonth(month))));
+		  events.addAll(await _loadPastData(DateSpan(from: month, to: Date.now()))); // TODO handle today better
+		  events.addAll(_loadFutureData(DateSpan(from: Date.now(), to: Utils.nextMonth(month))));
 	  }
-	  emit(state.copyWith(Utils.firstDayOfMonth(month), events));
+	  _allEvents.putIfAbsent(Utils.firstDayOfMonth(month), () => events);
+	  return events;
   }
 
-
   /// [span] - must fit within a month (from 1'st to the end)
-	Future<Map<Date, List<UIPlan>>> loadPastData(DateSpan<Date> span) async {
+	Future<Map<Date, List<UIPlan>>> _loadPastData(DateSpan<Date> span) async {
 		var instances = await _dataRepository.getPlanInstances(planIDs: _plans.keys.toList(), between: span);
 		var dateMap = groupBy<Date, PlanInstance>(instances, (plan) => plan.date);
 
 	  Map<Date, List<UIPlan>> events = {};
 		for (var entry in dateMap.entries) {
 			var planMap = groupBy<ObjectId, PlanInstance>(entry.value, (plan) => plan.planID);
-			events[entry.key] = planMap.keys.map((planId) => UIPlan.fromDBModel(_plans[planId], _getDescription(planMap[planId]))).toList();
+			events[entry.key] = planMap.keys.map((planId) => UIPlan.fromDBModel(_plans[planId])).toList();
 		}
 		return events;
 	}
 
-	Map<Date, List<UIPlan>> loadFutureData(DateSpan<Date> span) {
+	Map<Date, List<UIPlan>> _loadFutureData(DateSpan<Date> span) {
 		Map<Date, List<UIPlan>> events = {};
 		for (var plan in _plans.entries) {
 			var dates = _repeatabilityService.getRepeatabilityDatesInSpan(plan.value.repeatability, span);
 			for (var date in dates)
-				(events[date] ??= []).add(UIPlan.fromDBModel(plan.value, _getAssignedToDescription(plan.value.assignedTo.map((child) => _children[child]).toList())));
+				(events[date] ??= []).add(UIPlan.fromDBModel(plan.value));
 		}
 		return events;
 	}
 
-	DateSpan<Date> getMonthSpan(Date month) => DateSpan(from: month, to: Utils.nextMonth(month));
+	DateSpan<Date> _getMonthSpan(Date month) => DateSpan(from: month, to: Utils.nextMonth(month));
 
-	TranslateFunc _getDescription(List<PlanInstance> plans) {
-		return _getAssignedToDescription(plans.map((plan) => _children[plan.assignedTo]).toList());
+	TranslateFunc _getDescription(Iterable<ObjectId> children) {
+		return _getAssignedToDescription(children.map((id) => _childNames[id]));
 	}
 
 	TranslateFunc _getAssignedToDescription(List<String> children) {
@@ -85,12 +110,20 @@ class CalendarCubit extends Cubit<CalendarState> {
 }
 
 class CalendarState extends Equatable {
-	final Map<Date, Map<Date, List<UIPlan>>> events;
+	final Map<UIUser, bool> children;
+	final Date month;
+	final Map<Date, List<UIPlan>> events;
 
-	const CalendarState([this.events = const {}]);
+	const CalendarState({this.month, this.children, this.events});
 
-	CalendarState copyWith(Date month, Map<Date, List<UIPlan>> events) => CalendarState(this.events..putIfAbsent(month, () => events));
+	CalendarState copyWith({Map<UIUser, bool> children, Map<Date, List<UIPlan>> events, Date month}) {
+	  return CalendarState(
+		  children: children ?? this.children,
+		  events: events ?? this.events,
+		  month: month ?? this.month
+	  );
+	}
 
 	@override
-	List<Object> get props => [events];
+	List<Object> get props => [events, children, month];
 }
--- lib/model/ui/plan/ui_plan.dart
+++ lib/model/ui/plan/ui_plan.dart
@@ -11,8 +11,11 @@ class UIPlan extends UIPlanBase {
 	final List<ObjectId> assignedTo;
 
   UIPlan(ObjectId id, String name, this.isActive, this.taskCount, this.assignedTo, TranslateFunc description) : super(id, name, description);
-	UIPlan.fromDBModel(Plan plan, TranslateFunc description) : this(plan.id, plan.name, plan.active, plan.tasks.length, plan.assignedTo, description);
+	UIPlan.fromDBModel(Plan plan, [TranslateFunc description]) : this(plan.id, plan.name, plan.active, plan.tasks.length, plan.assignedTo, description);
+	UIPlan copyWith({ObjectId id, String name, bool isActive, int taskCount, List<ObjectId> assignedTo, TranslateFunc description}) {
+		return UIPlan(id ?? this.id, name ?? this.name, isActive ?? this.isActive, taskCount ?? this.taskCount, assignedTo ?? this.assignedTo, description ?? this.description);
+	}
 
-  @override
+	@override
   List<Object> get props => super.props..addAll([taskCount, assignedTo, isActive]);
 }

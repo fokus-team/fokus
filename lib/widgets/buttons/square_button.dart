1ed5065e8f5207f5c71c0ad4239b81d13226d3db
Mateusz <37753289+Evil00787@users.noreply.github.com>
2020-08-24 17:54:38 +0200

    Merge branch 'master' into feature/FKS-168
    # Conflicts:
    #	lib/model/ui/plan/ui_plan.dart
    #	lib/services/data/plan/plan_db_repository.dart
    #	lib/services/data/plan/plan_repository.dart

--- lib/widgets/buttons/square_button.dart
+++ lib/widgets/buttons/square_button.dart
@@ -2,23 +2,24 @@ import 'package:flutter/material.dart';
 
 class SquareButton extends StatelessWidget {
   final String title;
+  final bool isVertical;
   final IconData icon;
   final Function behaviour;
+  final Color backgroundColor;
 
-  SquareButton(this.title, this.icon, this.behaviour);
+  SquareButton(this.title, this.icon, this.behaviour, {this.isVertical = false, this.backgroundColor = Colors.white});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: FlatButton(
-        onPressed: behaviour,
-        child: Column(
-          children: [
-            Icon(icon),
-            Text(title)
-          ],
-        ),
-      )
-    );
+    return  FlatButton(
+			color: backgroundColor,
+			onPressed: behaviour,
+			child: Column(
+				children: [
+					Icon(icon),
+					Text(title)
+				],
+			),
+		);
   }
 }

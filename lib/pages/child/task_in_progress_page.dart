d16d8f6931c4a2527d2faf2b48bd08660dafe887
Mateusz <37753289+Evil00787@users.noreply.github.com>
2020-08-24 20:44:35 +0200

    Small adjustments

--- lib/pages/child/task_in_progress_page.dart
+++ lib/pages/child/task_in_progress_page.dart
@@ -17,92 +17,97 @@ class _TaskInProgressPageState extends State<TaskInProgressPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            AppHeader.widget(
-            title: '$_pageKey.header.title',
-            appHeaderWidget: ItemCard(
-                  title: "Sprzątanie pokoju",
-                  subtitle: "Co każdy poniedziałek, środę, czwartek i piątek",
-                  isActive: true,
-                  progressPercentage: 0.4,
-                  activeProgressBarColor: AppColors.childActionColor,
-                  chips:
-                  <Widget>[
-                    AttributeChip.withIcon(
-                        icon: Icons.description,
-                        color: AppColors.childBackgroundColor,
-                        content: AppLocales.of(context).translate('page.childSection.panel.content.taskProgress', {'NUM_TASKS': 2, 'NUM_ALL_TASKS': 5})
-                    )
-                  ],
-                ),
-          helpPage: 'plan_info'
-            ),
-          FlipCard(
-            front: Container(
-              decoration: BoxDecoration(
-                color: AppColors.childActionColor
-              ),
-              child: Column(
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.clear),
-                    onPressed: null,
-                  ),
-                  ListView(
-                    children: [
-                      Expanded(
-                        child: Text("xd"),
-                    ),
-                      Expanded(
-                       child: Text("xDddd"),
-                      ),
-                      Row(
-                        children: [
-                          SquareButton(
-                            "xd",
-                            Icons.clear,
-                            null
-                          ),
-                          SquareButton(
-                              "xd",
-                              Icons.clear,
-                              null
-                          )
-                        ],
-                      )
-                   ]
-                  ),
-            Row(
-              children: [
-                SquareButton(
-                    "xd",
-                    Icons.clear,
-                    null
-                ),
-                SquareButton(
-                    "xd",
-                    Icons.clear,
-                    null
-                ),
-                SquareButton(
-                    "xd",
-                    Icons.clear,
-                    null
-                )
+  	return Scaffold(
+			body: Column(
+				crossAxisAlignment: CrossAxisAlignment.start,
+				children: [
+					AppHeader.widget(
+						title: '$_pageKey.header.title',
+						appHeaderWidget: ItemCard(
+							title: "Sprzątanie pokoju",
+							subtitle: "Co każdy poniedziałek, środę, czwartek i piątek",
+							isActive: true,
+							progressPercentage: 0.4,
+							activeProgressBarColor: AppColors.childActionColor,
+							chips:
+							<Widget>[
+								AttributeChip.withIcon(
+									icon: Icons.description,
+									color: AppColors.childBackgroundColor,
+									content: AppLocales.of(context).translate('page.childSection.panel.content.taskProgress', {'NUM_TASKS': 2, 'NUM_ALL_TASKS': 5})
+								)
+							],
+						),
+						helpPage: 'plan_info'
+					),
+					FlipCard(
+						front: Container(
+							decoration: BoxDecoration(
+								color: AppColors.childActionColor
+							),
+							child: Column(
+								children: [
+									IconButton(
+										icon: Icon(Icons.clear),
+										onPressed: null,
+									),
+									ListView(
+										children: [
+											Expanded(
+												child: Text("xd"),
+											),
+											Expanded(
+												child: Text("xDddd"),
+											),
+											Row(
+												children: [
+													SquareButton(
+														"xd",
+														Icons.clear,
+														null
+													),
+													SquareButton(
+														"xd",
+														Icons.clear,
+														null
+													)
+												],
+											)
+										]
+									),
+									Positioned(
+										right: 0,
+										left: 0,
+										bottom: 0,
+										child: Row(
+											children: [
+												SquareButton(
+													"xd",
+													Icons.clear,
+													null
+												),
+												SquareButton(
+													"xd",
+													Icons.clear,
+													null
+												),
+												SquareButton(
+													"xd",
+													Icons.clear,
+													null
+												)
+											],
+										),
+									),
+								]
+							),
+						),
+						back: Container(
 
-              ],
-              ),
-            ]),
-    ),
-            back: Container(
-              
-            ),
-          )
-          ]
-      )
-    );
+						),
+					)
+				]
+			)
+		);
   }
 }

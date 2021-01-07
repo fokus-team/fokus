d63f6e21485ff724c7568c9810965f8f3f8d5299
Mikolaj Mirko <mikolajmirko@gmail.com>
2021-01-07 01:02:08 +0100

    Better chip tooltip message function

--- lib/logic/caregiver/caregiver_friends_cubit.dart
+++ lib/logic/caregiver/caregiver_friends_cubit.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fokus/logic/common/auth_bloc/authentication_bloc.dart';
 import 'package:fokus/logic/common/formz_state.dart';
